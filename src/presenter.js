ab67f0301c54c759cdd0cd959ace4ab0b71fb40f
Jorge Vargas <jorge.vargas.p@ucb.edu.bo>
2024-03-06 15:04:34 -0400

    agregar precio

--- src/presenter.js
+++ src/presenter.js
@@ -1,10 +1,12 @@
 import bisiesto from "./Bisiesto.js";
 const cantidad = document.querySelector("#cantidad-input");
+const precio = document.querySelector("#precio-input");
 const form = document.querySelector("#totalizador-form");
 const resultadoDiv = document.querySelector("#resultado-div");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const cant = Number.parseInt(cantidad.value);
-  resultadoDiv.innerHTML = "<p>" + cant + "</p>";
+  const prec = Number.parseInt(precio.value);
+  resultadoDiv.innerHTML = "<p>Cantidad:  " + cant + "</p>" + "<p> Precio:  "+ prec+"</p>";
 });
