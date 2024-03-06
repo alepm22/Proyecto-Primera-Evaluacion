1b32bf0642a0b59c141545ca779ea286d38df487
Jorge Vargas <jorge.vargas.p@ucb.edu.bo>
2024-03-06 16:24:59 -0400

    precio con descuento

--- src/sumador.js
+++ src/sumador.js
@@ -1,6 +1,14 @@
 function Calcular (precio,cantidad,TI) {
   const precio_neto= precio * cantidad;
+  if(precio_neto>=1000){
+
+    const newprecio_neto = precio_neto-(precio_neto * 0.03);
+    const impuesto = newprecio_neto * (TI/100);
+    const totalimpuesto = newprecio_neto + impuesto;
+    return totalimpuesto;
+  }
   const impuesto = precio_neto * (TI/100);
-  return precio_neto + impuesto;
+  const totalimpuesto = precio_neto + impuesto;
+  return totalimpuesto;
 }
 export default Calcular;
