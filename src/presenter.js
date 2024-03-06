f7b32598ab094034107e803a762a7f15de6735dd
Jorge Vargas <jorge.vargas.p@ucb.edu.bo>
2024-03-06 14:59:40 -0400

    agregar la cantidad

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,10 @@
-import sumar from "./sumador";
-
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
-
+import bisiesto from "./Bisiesto.js";
+const cantidad = document.querySelector("#cantidad-input");
+const form = document.querySelector("#totalizador-form");
+const resultadoDiv = document.querySelector("#resultado-div");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  const cant = Number.parseInt(cantidad.value);
+  resultadoDiv.innerHTML = "<p>" + cant + "</p>";
 });
