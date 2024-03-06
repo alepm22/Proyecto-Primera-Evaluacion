c862c8119e1122a2ced485e694b38e98ea4e08a7
Jorge Vargas <jorge.vargas.p@ucb.edu.bo>
2024-03-06 15:40:52 -0400

    precio impuesto tx

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -1,7 +1,10 @@
 import Calcular from "./Bisiesto.js";
 
 describe("Calcular Total", () => {
-  it("Esta bien si:", () => {
+  it("Esta bien CA:", () => {
     expect(Calcular(20,3,8.25)).toEqual(64.95);
+  });
+it("Esta bien TX:", () => {
+  expect(Calcular(20,3,6.25)).toEqual(63.75);
   });
 });
